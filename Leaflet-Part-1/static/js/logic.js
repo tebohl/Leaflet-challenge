1805ce841bfc4ae37e28e042bc0ed3d56807381a
taylorbohl <tbohl3@gmail.com>
2022-06-18 16:57:53 -0500

    adding to logic.js and readme

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,61 @@
+// Define a function we want to run once for each feature in the features array
+// Give each feature a popup describing the place and time of the earthquake
+function popUpMsg(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+}
+
+ // Define streetmap and darkmap layers
+var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1
+  });
+
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    maxZoom: 18
+  });
+
+
+// Define a baseMaps object to hold our base layers
+var baseMaps = {
+    "Street Map": streetmap,
+    "Topographic Map": topo
+    };
+
+// Create our map, giving it the streetmap and earthquakes layers to display on load
+var myMap = L.map("map", {
+    center: [ 37.09, -95.71 ],
+    zoom: 5,
+    layers: [streetmap]     //default selected layer
+    });
+
+// create layer; will attach data later on
+var earthquakes = L.layerGroup();
+
+// Create overlay object to hold our overlay layer
+var overlayMaps = {
+  Earthquakes: earthquakes
+};
+
+// Create a layer control
+// Pass in our baseMaps and overlayMaps
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
+
+// Store our API endpoint inside queryUrl
+queryURL= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(data) {
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    L.geoJSON(data, {
+      onEachFeature: popUpMsg
+    }).addTo(earthquakes);
+  
+    earthquakes.addTo(myMap);
+});
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # leaflet-challenge
+
+In this assignment, I used javascript and Leaflet to visualize an earthquake dataset from USGS. All of the earthquakes from the last 7 days are shown on the map at their respective lat/long points. I added data markers to reflect the magnitude of the earthquake by size and the depth of the earthquake by color. Earthquakes with higher magnitudes appear larger, and earthquakes with greater depth should appear darker in color. Included are popups that provide additional information about the earthquake when its associated marker is clickedas well as a legend to help provide context.
+
+
+Additionally, I plotted a second dataset on the map to illustrate the relationship between tectonic plates and seismic activity. Data used for tectonic plates can be found at https://github.com/fraxen/tectonicplates. The tectonic plates dataset is plotted on the map in addition to the earthquakes but each dataset is in a separate overlay that can be turned on and off independently. Screenshots of the final page are below.
